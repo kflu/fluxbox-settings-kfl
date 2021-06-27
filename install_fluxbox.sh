d0118866bbcf2dbad168974a1ad5db4dbd4275ff
Kefei Lu <kflu@users.noreply.github.com>
2021-06-26 23:25:17 -0700

    Only install absolutely necessary packages
    I found packages keep changing names so this often fails. Reducing to absolutely needed packages.

--- install_fluxbox.sh
+++ install_fluxbox.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 dnf install fluxbox \
-            gmrun \
-            rxvt-unicode-256color-ml \
             xterm \
-            chromium-browser \
             xclip \
-            xpdf \
 	    tigervnc-server \
 	    libXau libXext
