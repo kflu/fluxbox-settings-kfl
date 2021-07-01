12a25ba3bef851c14adabb968e1ea4f5e8de57b9
KFL <kludev@gmail.com>
2021-07-01 00:00:34 -0700

    _

--- install_fluxbox.sh
+++ install_fluxbox.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-dnf install -y fluxbox \
-               xterm \
-               xclip \
-               tigervnc-server \
-               libXau libXext
+# necessary:
+if dnf install -y fluxbox xterm libXau libXext tigervnc-server
+then
+    echo "NECESSARY FLUXBOX COMPONENTS INSTALLED SUCCESSFULLY"
+else
+    echo "NECESSARY FLUXBOX COMPONENTS CANNOT BE INSTALLED"
+    exit 1
+fi
+
+# optional:
+dnf install -y rxvt-unicode xclip || \
+    echo "SOME OPTIONAL COMPONENTS NOT CANNOT BE INSTALLED"
+
